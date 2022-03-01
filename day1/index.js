c4fda4bd302ee30bbf5f9089327fea400d4e5c09
ram chandra <ramachandra.node@gamil.com>
2022-03-01 15:27:47 +0530

    day1 traing

---
+++ day1/index.js
@@ -0,0 +1,4 @@
+const a =100;
+console.log(a);
+a=100;
+console.log(a); // throws erros
---
+++ day1/number.js
@@ -0,0 +1,14 @@
+let a =10;
+console.log('line-2',a);
+console.log(typeof a);
+a= a.toString();
+console.log('line-4',a)
+console.log(typeof a);
+
+
+//toFixed()
+
+
+let a =13.455666
+console.log(a.toFixed(2))
+console.log(a.toFixed(4))
